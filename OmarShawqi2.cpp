6e986e5247c3736769cc91198c4843d6265c50c8
Omar Shawqi <145785192+OMARSHAWQI7722@users.noreply.github.com>
2024-10-31 15:35:19 +0300

    Add files via upload
    Many programs in one project we use it to understand how we can use switch in different situations.
    Done by OMAR SHAWQI.
    Thanks.

---
+++ OmarShawqi2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+	 int a,b;
+	 
+	 
+	 
+ cout<<"Enter The Number:";
+ cin>>a;
+ 
+ cout<<"Enter The password:";
+ cin>>b;
+ 
+ if(a>0){
+ 	
+ 	cout<<"The number is posative:"<<a;}
+ 	
+ 	else {
+ 		
+ 		cout<<"Error!! The number is incorrect";
+ 		
+ 	
+	 }
+	 	return 0;
+	 	
+	 	
+	 	cout<<"Numbers is :"<< a + b <<endl;
+ 
+	
+	
+}
