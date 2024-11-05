5ac5505bc3c335699b59cfcea5322d9a64a82f2b
Omar Shawqi <145785192+OMARSHAWQI7722@users.noreply.github.com>
2024-11-05 19:18:57 +0300

    Add files via upload
    Program to summation of even numbers for 5 num that enter by user
    
    Omar Shawqi

---
+++ number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int num;
+	int sum=0;
+	
+	cout<<"Enter 5 Numbers:";
+
+	for(int i =0; i <5; i++){
+			cin>>num;
+		if(num %2 ==0){
+			
+			sum+=num;
+			
+			
+			
+		}
+		
+		}
+		
+			cout<<"The summation of even numbers is:"<<sum<<endl;
+	
+	}
+	
+	
